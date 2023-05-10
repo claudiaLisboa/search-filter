18804e9628160f3ce943dd0e175dc2d08d51548e
Claudia Lisboa <ccalumby@gmail.com>
2023-05-10 14:03:07 -0400

    first commit

---
+++ search-filter/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className='app'>
+       <input type='text' placeholder='Search...' className='search' />
+       <ul className='list'>
+        <li className='listItem'> Filo</li>
+        <li className='listItem'> Farofa</li>
+        <li className='listItem'> JoseAlves</li>
+       </ul>
+    </div>
+  )
+}
+
+export default App
